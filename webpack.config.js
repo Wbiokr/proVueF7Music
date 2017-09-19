c931025f48ce22f5dd26eb86407028d9a88ea25c
wbiokr <Wbiokr@hotmail.com>
2017-09-19 11:14:00 +0800

    first

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 var path = require('path')
 var webpack = require('webpack')
+const ExtractTextPlugin=require('extract-text-webpack-plugin')
+const ipv4=require('ipv4')
+
+
 
 module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
-    filename: 'build.js'
+    publicPath: './dist/',
+    filename: 'script/build.js'
   },
   module: {
     rules: [
@@ -14,10 +18,24 @@ module.exports = {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
-          loaders: {
-          }
-          // other vue-loader options go here
+          extractCSS: true,
+          loaders:[
+            {
+              test: /\.stylus/,
+              use: ExtractTextPlugin.extract({
+                fallback:'vue-style-loader',
+                use:['css-loader','stylus-loader']
+              })
+            }
+          ]
         }
+      },
+      {
+        test:/\.stylus/,
+        use:ExtractTextPlugin.extract({
+          fallback:'style-loader',
+          use:['css-loader','stylus-loader']
+        })
       },
       {
         test: /\.js$/,
@@ -28,11 +46,12 @@ module.exports = {
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'file-loader',
         options: {
-          name: '[name].[ext]?[hash]'
+          name: 'img/[name].[ext]'
         }
       }
     ]
   },
+
   resolve: {
     alias: {
       'vue$': 'vue/dist/vue.esm.js'
@@ -40,16 +59,23 @@ module.exports = {
   },
   devServer: {
     historyApiFallback: true,
-    noInfo: true
+    noInfo: true,
+    port:Math.floor(Math.random()*10000),
+    host:ipv4||'127.0.0.1'
   },
   performance: {
     hints: false
   },
-  devtool: '#eval-source-map'
+  devtool: '#eval-source-map',
+  plugins:[
+    new ExtractTextPlugin({
+      filename:'style/app.css'
+    }),
+  ]
 }
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
+  module.exports.devtool = '#cheap-source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
@@ -65,6 +91,7 @@ if (process.env.NODE_ENV === 'production') {
     }),
     new webpack.LoaderOptionsPlugin({
       minimize: true
-    })
+    }),
+    
   ])
 }
