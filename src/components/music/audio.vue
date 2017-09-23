b748fc3bca2f25988339510ea8a9f043af3c72cd
wbiokr <Wbiokr@hotmail.com>
2017-09-23 11:10:52 +0800

    audio修改

--- src/components/music/audio.vue
+++ src/components/music/audio.vue
@@ -1,6 +1,6 @@
 <template lang='jade'>
   div.audio   
-    audio(:src='music'  controls loop ref='audio') 您的浏览器不支持audio标签
+    audio(:src='music'  controls loop ref='audio' ontimeupdate='change') 您的浏览器不支持audio标签
     slot
 </template>
 <style lang="stylus">
@@ -9,9 +9,13 @@
 </style>
 <script>
   // import vList from '../components/music/list.vue';
+  function change(){
+    console.log(document.querySelector('audio').currentTime)
+  }
   export default{
     data(){return{}},
     // components:{vList},
+
     computed:{
       music(){
         return this.$store.state.music.musicFormatSrc;
@@ -25,15 +29,24 @@
     },
     mounted(){
       this.isPlay?this.$refs.audio.play():this.$refs.audio.pause();
+      this.rangeChagne();
     },
     watch:{
       isPlay(newV,oldV){
-        console.log(newV,oldV);
         if(newV){
           this.$refs.audio.play()
         }else{
           this.$refs.audio.pause()
-        }
+        };
+        console.log(this.$refs.audio.currentTime)
+      }
+    },
+    methods:{
+      timeupdate(){
+        console.log(this.$refs.audio.currentTime)
+      },
+      rangeChagne(){
+        console.log(this.$refs.audio.currentTime)
       }
     }
   }
