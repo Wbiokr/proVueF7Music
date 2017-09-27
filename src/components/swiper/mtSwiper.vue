370e2afb9b0a7f9e3951bb2b87d1ad29e49de959
wbiokr <Wbiokr@hotmail.com>
2017-09-27 23:19:17 +0800

    banner

--- src/components/swiper/mtSwiper.vue
+++ src/components/swiper/mtSwiper.vue
@@ -1,23 +1,45 @@
-<template lang=''>
-    <carousel :indicators="true" :auto="3000" @onSlideEnd='endcall'>
-        <div v-for="i in 3">carousel-item-{{i-1}}</div>
-    </carousel>
+<template lang='jade'>
+  carousel(:indicators="true" v-bind:auto="3000" @onSlideEnd='endcall' )
+    div(:key='i' v-for="i in banner"  class='swip-item' ) 
+      img(:src="i") 
 </template>
 <script>
-import Carousel from 'vue-m-carousel'
+  import Carousel from 'vue-m-carousel';
+  import img0 from './img/swip_0.jpg';
+  import img1 from './img/swip_1.jpg';
+  import img2 from './img/swip_2.jpg';
+  import img3 from './img/swip_3.jpg';
   export default {
     components: {
       Carousel
     },
     data() {
       return {
-        
+        banner:[
+          img0,img1,img2,img3
+        ]
       }
     },
     methods: {
-      endcall(){
+      endcall() {
         console.log(2121)
       }
+    },
+    mounted(){
+      [...document.querySelectorAll('.carousel-dot')].map(v=>v.innerHTML='')
     }
   }
 </script>
+<style lang="stylus">
+  .carousel-dot
+    // color orange
+    // content ''
+    border-radius 50%
+    transform  scale(.8)
+  .swip-item
+    width 100%
+    img
+      width 100%
+      height 100%
+      margin-top -20px
+</style>
